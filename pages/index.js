174ad1f47594decf90644caf4559ac0b07138111
yamato0211 <kumayama0211@gmail.com>
2022-05-03 20:34:40 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.text}>FastApi</h1>
+        <h1 className={styles.gap}>完全に理解した</h1>
+      </div>
+    </div>
+  )
+}
